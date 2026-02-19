547164b463a7e662b12f8890ae097b8feef3547a
SiriusKoan <me@siriuskoan.one>
2026-02-19 17:23:01 +0800

    feat: use exim for mailer in demo

--- demo/docker/mailer/entrypoint.sh
+++ demo/docker/mailer/entrypoint.sh
@@ -1,14 +1,10 @@
 #!/bin/sh
 set -e
 
-mkdir -p /var/spool/postfix/dev
-ln -sf /dev/log /var/spool/postfix/dev/log
-cp /etc/resolv.conf /var/spool/postfix/etc/resolv.conf
-
 rsyslogd
 /usr/sbin/sshd
 
-postfix start
+exim4 -bd &
 vector -c /etc/vector/vector.yaml &
 
 tail -F /var/log/mail.log
